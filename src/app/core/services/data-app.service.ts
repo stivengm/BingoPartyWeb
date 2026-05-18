000cb33092ec0aae2265ac492355837f0fe2c859
Stiven Gomez <stivenalexander7u7@gmail.com>
2026-05-18 17:10:43 -0500

    FIX - Ajuste para almacenado de información en el sessionStorage.

--- src/app/core/services/data-app.service.ts
+++ src/app/core/services/data-app.service.ts
@@ -7,11 +7,26 @@ import { Player } from '../models/player.model';
 })
 export class DataAppService {
 
+    public saveStorage(key: string, data: any) {
+        sessionStorage.setItem(key, JSON.stringify(data));
+    }
+
+    public getStorage<T>(key: string): T | null {
+        const data = sessionStorage.getItem(key);
+
+        if (!data) {
+            return null;
+        }
+
+        return JSON.parse(data) as T;
+    }
+
     //#region Evento para almacenar el alias de la persona
     private $player = new BehaviorSubject<Player | null>(null);
 
     public setPlayer(val: Player): void {
         this.$player.next(val);
+        this.saveStorage('player', val);
     }
 
     public getPlayer(): Observable<any> {
--- src/app/features/welcome-view/welcome-view.ts
+++ src/app/features/welcome-view/welcome-view.ts
@@ -3,26 +3,44 @@ import { Version } from "../../shared/version/version";
 import { Bingo } from '../../shared/bingo/bingo';
 import { DataAppService } from '../../core/services/data-app.service';
 import { Player } from '../../core/models/player.model';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-welcome-view',
   imports: [
     Bingo,
-    Version
+    Version,
+    ReactiveFormsModule
   ],
   templateUrl: './welcome-view.html',
   styleUrl: './welcome-view.scss',
 })
 export class WelcomeView {
 
+  public aliasControl = new FormControl(
+    '',
+    [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(20)
+    ]
+  );
+
   constructor(private dataApp: DataAppService) {
 
   }
 
   selectUser(isHost: boolean = false) {
+    if (this.aliasControl.invalid) {
+      this.aliasControl.markAsTouched();
+      return;
+    }
+
+    const alias = this.aliasControl.value;
+
     const player: Player = {
       id: "123",
-      name: "",
+      name: alias ?? "",
       isHost: isHost
     }
 
