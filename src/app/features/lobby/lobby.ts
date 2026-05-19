d8bac7a67c194f95f595b569bd171a25c2547693
Stiven Gomez <stivenalexander7u7@gmail.com>
2026-05-18 19:33:17 -0500

    FIX - Ajuste de maquetación lobby

--- src/app/features/lobby/lobby.ts
+++ src/app/features/lobby/lobby.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { Header } from '../../shared/layout/header/header';
 
 @Component({
   selector: 'app-lobby',
-  imports: [],
+  imports: [
+    Header
+  ],
   templateUrl: './lobby.html',
   styleUrl: './lobby.scss',
 })
