e7ae5c3f772e80658c8dbf030e9f8bcf0555894f
Stiven Gomez <stivenalexander7u7@gmail.com>
2026-05-18 21:21:09 -0500

    FIX - Ajuste de detalles para elegir el tipo de juego.

--- src/app/features/create-room/create-room.ts
+++ src/app/features/create-room/create-room.ts
@@ -1,20 +1,32 @@
 import { Component } from '@angular/core';
 import { Header } from '../../shared/layout/header/header';
 import { Router } from '@angular/router';
+import { CommonModule, NgClass } from "@angular/common";
 
 @Component({
   selector: 'app-create-room',
   imports: [
-    Header
-  ],
+    CommonModule,
+    Header,
+    NgClass
+],
   templateUrl: './create-room.html',
   styleUrl: './create-room.scss',
 })
 export class CreateRoom {
 
+  idGameType = 0;
+
   constructor(private router: Router) {}
 
   createRoom() {
     this.router.navigate(['lobby']);
+  }
+
+  selectedGameType(id: number) {
+    debugger;
+
+    this.idGameType = id;
+
   }
 }
