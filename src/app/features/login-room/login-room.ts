c71946f50f7b5a5273889d46c87e12dc5e1177af
Stiven Gomez <stivenalexander7u7@gmail.com>
2026-05-18 19:08:37 -0500

    FIX - Ajuste de funcionalidad para conectar el teclado con el input.

--- src/app/features/login-room/login-room.ts
+++ src/app/features/login-room/login-room.ts
@@ -2,24 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { Header } from '../../shared/layout/header/header';
 import { Player } from '../../core/models/player.model';
 import { DataAppService } from '../../core/services/data-app.service';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-room',
   imports: [
-    Header
+    Header,
+    ReactiveFormsModule
   ],
   templateUrl: './login-room.html',
   styleUrl: './login-room.scss',
 })
 export class LoginRoom implements OnInit {
   player: Player | null = null;
+  public roomControl = new FormControl('');
 
   public keys: string[] = [
     '1', '2', '3',
     '4', '5', '6',
     '7', '8', '9',
     'CLEAN', '0', 'DEL'
-];
+  ];
 
 
   constructor(private dataApp: DataAppService) {
@@ -34,8 +37,26 @@ export class LoginRoom implements OnInit {
     });
   }
 
-  pressKey(key: string) {
+  pressKey(key: string): void {
+    const currentValue = this.roomControl.value || '';
 
+    switch (key) {
+      case 'DEL':
+        this.roomControl.setValue(
+          currentValue.slice(0, -1)
+        );
+        break;
+      case 'CLEAN':
+        this.roomControl.setValue('');
+        break;
+      default:
+        this.roomControl.setValue(
+          currentValue + key
+        );
+        break;
+    }
+
+    console.log(this.roomControl.value);
   }
 
 }
