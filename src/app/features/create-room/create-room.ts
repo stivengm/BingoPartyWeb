2eda664fc7de8fa1dde9a333dde1904cae659507
Stiven Gomez <stivenalexander7u7@gmail.com>
2026-05-19 18:26:29 -0500

    FIX - Ajuste de validaciones de tipo de juego y estilo de cartón, y obtener el id de la sala automáticamente.

--- src/app/features/create-room/create-room.ts
+++ src/app/features/create-room/create-room.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { CommonModule, NgClass } from "@angular/common";
 import { TableroBingo } from '../../shared/tablero-bingo/tablero-bingo';
 import { DataAppService } from '../../core/services/data-app.service';
+import { errorModal } from '../../utils/modals';
 
 @Component({
   selector: 'app-create-room',
@@ -21,17 +22,36 @@ export class CreateRoom {
   idGameType = 0;
   idBoardType = 0;
 
+  idRoom = "";
+
   boards = [1, 2, 3, 4, 5, 6, 7, 8];
 
   constructor(
     private dataApp: DataAppService
   ) {}
 
+  ngOnInit() {
+    this.idRoom = Array.from({ length: 6 }, () =>
+      Math.floor(Math.random() * 10)
+    ).join('');
+  }
+
   returnWelcome() {
     this.dataApp.goToPage("/welcome_view");
   }
 
   createRoom() {
+    if (this.idGameType === 0) {
+      errorModal({ title: "Por favor eliga el tipo de juego."});
+      return;
+    }
+
+    if (this.idBoardType === 0) {
+      errorModal({ title: "Por favor eliga el estilo de cartón."});
+      return;
+    }
+
+    // TODO: Realizar la creación de la sala.
     this.dataApp.goToPage("/lobby");
   }
 
