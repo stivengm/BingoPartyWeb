f6c5aefdc2aedcd4f4e7f54cdca18fbd99402f5a
Stiven Gomez <stivenalexander7u7@gmail.com>
2026-05-20 22:42:08 -0500

    ADD - Se añade funcionalidad de validar las casillas una por una.

--- src/app/core/models/verify_cell.model.ts
+++ src/app/core/models/verify_cell.model.ts
@@ -1,9 +1,17 @@
 type VerifyStatus =
   | 'pending'
   | 'success'
-  | 'error';
-  
-interface VerifyCell {
+  | 'error'
+  | 'active-success'
+  | 'active-error';
+
+export interface VerifyBall {
+  id: number;
+  isComplete: boolean;
+  status: VerifyStatus;
+}
+
+export interface VerifyCell {
   number: number;
   isDrawn: boolean;
   status: VerifyStatus;
--- src/app/shared/verify-game-results/verify-game-results.ts
+++ src/app/shared/verify-game-results/verify-game-results.ts
@@ -1,21 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 
 import { DataAppService } from '../../core/services/data-app.service';
 
 import { Player } from '../../core/models/player.model';
 import { BingoCell } from '../../core/models/bingo_cell.model';
+import { VerifyBall, VerifyCell } from '../../core/models/verify_cell.model';
 
-type VerifyStatus =
-  | 'pending'
-  | 'success'
-  | 'error';
+// type VerifyStatus =
+//   | 'pending'
+//   | 'success'
+//   | 'error'
+//   | 'active-success'
+//   | 'active-error';
 
-interface VerifyCell {
-  number: number;
-  isDrawn: boolean;
-  status: VerifyStatus;
-}
+// interface VerifyBall {
+//   id: number;
+//   isComplete: boolean;
+//   status: VerifyStatus;
+// }
+
+// interface VerifyCell {
+//   number: number;
+//   isDrawn: boolean;
+//   status: VerifyStatus;
+// }
 
 @Component({
   selector: 'app-verify-game-results',
@@ -33,109 +43,104 @@ export class VerifyGameResults implements OnInit {
 
   boardVerify: VerifyCell[][] = [];
 
-  allBalls = [
-    { id: 1, isComplete: true },
-    { id: 2, isComplete: false },
-    { id: 3, isComplete: true },
-    { id: 4, isComplete: true },
-    { id: 5, isComplete: false },
-    { id: 6, isComplete: true },
-    { id: 7, isComplete: false },
-    { id: 8, isComplete: false },
-    { id: 9, isComplete: true },
-    { id: 10, isComplete: false },
+  allBalls: VerifyBall[] = [
+    { id: 1, isComplete: true, status: 'pending' },
+    { id: 2, isComplete: false, status: 'pending' },
+    { id: 3, isComplete: true, status: 'pending' },
+    { id: 4, isComplete: true, status: 'pending' },
+    { id: 5, isComplete: false, status: 'pending' },
+    { id: 6, isComplete: true, status: 'pending' },
+    { id: 7, isComplete: false, status: 'pending' },
+    { id: 8, isComplete: false, status: 'pending' },
+    { id: 9, isComplete: true, status: 'pending' },
+    { id: 10, isComplete: false, status: 'pending' },
 
-    { id: 11, isComplete: false },
-    { id: 12, isComplete: true },
-    { id: 13, isComplete: false },
-    { id: 14, isComplete: false },
-    { id: 15, isComplete: true },
-    { id: 16, isComplete: false },
-    { id: 17, isComplete: false },
-    { id: 18, isComplete: true },
-    { id: 19, isComplete: false },
-    { id: 20, isComplete: false },
+    { id: 11, isComplete: false, status: 'pending' },
+    { id: 12, isComplete: true, status: 'pending' },
+    { id: 13, isComplete: false, status: 'pending' },
+    { id: 14, isComplete: false, status: 'pending' },
+    { id: 15, isComplete: true, status: 'pending' },
+    { id: 16, isComplete: false, status: 'pending' },
+    { id: 17, isComplete: false, status: 'pending' },
+    { id: 18, isComplete: true, status: 'pending' },
+    { id: 19, isComplete: false, status: 'pending' },
+    { id: 20, isComplete: false, status: 'pending' },
 
-    { id: 21, isComplete: true },
-    { id: 22, isComplete: false },
-    { id: 23, isComplete: true },
-    { id: 24, isComplete: true },
-    { id: 25, isComplete: false },
-    { id: 26, isComplete: false },
-    { id: 27, isComplete: true },
-    { id: 28, isComplete: false },
-    { id: 29, isComplete: false },
-    { id: 30, isComplete: true },
+    { id: 21, isComplete: true, status: 'pending' },
+    { id: 22, isComplete: false, status: 'pending' },
+    { id: 23, isComplete: true, status: 'pending' },
+    { id: 24, isComplete: true, status: 'pending' },
+    { id: 25, isComplete: false, status: 'pending' },
+    { id: 26, isComplete: false, status: 'pending' },
+    { id: 27, isComplete: true, status: 'pending' },
+    { id: 28, isComplete: false, status: 'pending' },
+    { id: 29, isComplete: false, status: 'pending' },
+    { id: 30, isComplete: true, status: 'pending' },
 
-    { id: 31, isComplete: false },
-    { id: 32, isComplete: false },
-    { id: 33, isComplete: true },
-    { id: 34, isComplete: false },
-    { id: 35, isComplete: false },
-    { id: 36, isComplete: true },
-    { id: 37, isComplete: true },
-    { id: 38, isComplete: false },
-    { id: 39, isComplete: true },
-    { id: 40, isComplete: true },
+    { id: 31, isComplete: false, status: 'pending' },
+    { id: 32, isComplete: false, status: 'pending' },
+    { id: 33, isComplete: true, status: 'pending' },
+    { id: 34, isComplete: false, status: 'pending' },
+    { id: 35, isComplete: false, status: 'pending' },
+    { id: 36, isComplete: true, status: 'pending' },
+    { id: 37, isComplete: true, status: 'pending' },
+    { id: 38, isComplete: false, status: 'pending' },
+    { id: 39, isComplete: true, status: 'pending' },
+    { id: 40, isComplete: true, status: 'pending' },
 
-    { id: 41, isComplete: false },
-    { id: 42, isComplete: true },
-    { id: 43, isComplete: false },
-    { id: 44, isComplete: false },
-    { id: 45, isComplete: true },
-    { id: 46, isComplete: true },
-    { id: 47, isComplete: true },
-    { id: 48, isComplete: true },
-    { id: 49, isComplete: false },
-    { id: 50, isComplete: false },
+    { id: 41, isComplete: false, status: 'pending' },
+    { id: 42, isComplete: true, status: 'pending' },
+    { id: 43, isComplete: false, status: 'pending' },
+    { id: 44, isComplete: false, status: 'pending' },
+    { id: 45, isComplete: true, status: 'pending' },
+    { id: 46, isComplete: true, status: 'pending' },
+    { id: 47, isComplete: true, status: 'pending' },
+    { id: 48, isComplete: true, status: 'pending' },
+    { id: 49, isComplete: false, status: 'pending' },
+    { id: 50, isComplete: false, status: 'pending' },
 
-    { id: 51, isComplete: true },
-    { id: 52, isComplete: false },
-    { id: 53, isComplete: false },
-    { id: 54, isComplete: true },
-    { id: 55, isComplete: false },
-    { id: 56, isComplete: false },
-    { id: 57, isComplete: true },
-    { id: 58, isComplete: true },
-    { id: 59, isComplete: true },
-    { id: 60, isComplete: true },
+    { id: 51, isComplete: true, status: 'pending' },
+    { id: 52, isComplete: false, status: 'pending' },
+    { id: 53, isComplete: false, status: 'pending' },
+    { id: 54, isComplete: true, status: 'pending' },
+    { id: 55, isComplete: false, status: 'pending' },
+    { id: 56, isComplete: false, status: 'pending' },
+    { id: 57, isComplete: true, status: 'pending' },
+    { id: 58, isComplete: true, status: 'pending' },
+    { id: 59, isComplete: true, status: 'pending' },
+    { id: 60, isComplete: true, status: 'pending' },
 
-    { id: 61, isComplete: false },
-    { id: 62, isComplete: false },
-    { id: 63, isComplete: true },
-    { id: 64, isComplete: false },
-    { id: 65, isComplete: true },
-    { id: 66, isComplete: true },
-    { id: 67, isComplete: false },
-    { id: 68, isComplete: true },
-    { id: 69, isComplete: true },
-    { id: 70, isComplete: false },
+    { id: 61, isComplete: false, status: 'pending' },
+    { id: 62, isComplete: false, status: 'pending' },
+    { id: 63, isComplete: true, status: 'pending' },
+    { id: 64, isComplete: false, status: 'pending' },
+    { id: 65, isComplete: true, status: 'pending' },
+    { id: 66, isComplete: true, status: 'pending' },
+    { id: 67, isComplete: false, status: 'pending' },
+    { id: 68, isComplete: true, status: 'pending' },
+    { id: 69, isComplete: true, status: 'pending' },
+    { id: 70, isComplete: false, status: 'pending' },
 
-    { id: 71, isComplete: false },
-    { id: 72, isComplete: true },
-    { id: 73, isComplete: false },
-    { id: 74, isComplete: false },
-    { id: 75, isComplete: true },
+    { id: 71, isComplete: false, status: 'pending' },
+    { id: 72, isComplete: true, status: 'pending' },
+    { id: 73, isComplete: true, status: 'pending' },
+    { id: 74, isComplete: false, status: 'pending' },
+    { id: 75, isComplete: true, status: 'pending' },
   ];
 
   constructor(
-    private dataApp: DataAppService
+    private dataApp: DataAppService,
+    private cdr: ChangeDetectorRef
   ) {}
 
   async ngOnInit(): Promise<void> {
-
     this.loadPlayer();
-
     await this.loadBoard();
-
     await this.startVerification();
-
   }
 
   loadPlayer(): void {
-
     this.dataApp.getPlayer().subscribe((player) => {
-
       if (player != null) {
         this.player = player;
         return;
@@ -152,82 +157,72 @@ export class VerifyGameResults implements OnInit {
       this.dataApp.setPlayer(playerStorage);
 
     });
-
   }
 
   async loadBoard(): Promise<void> {
-
     return new Promise((resolve) => {
-
       this.dataApp.getBoard().subscribe((board) => {
-
         if (board) {
-
           this.board = board;
-
           this.generateBoardVerify();
-
           resolve();
-
           return;
         }
-
         const boardStorage = this.dataApp.getStorage('board') as BingoCell[][];
-
         if (boardStorage) {
-
           this.board = boardStorage;
-
           this.generateBoardVerify();
-
           resolve();
-
         }
-
       });
-
     });
-
   }
 
   generateBoardVerify(): void {
-
     this.boardVerify = this.board.map((row) =>
-
       row.map((cell) => {
-
         const drawnBall = this.allBalls.find(
           ball => ball.id === cell.value
         );
-
         return {
           number: Number(cell.value),
           isDrawn: drawnBall?.isComplete ?? false,
           status: 'pending'
         };
-
       })
-
     );
-
   }
 
   async startVerification(): Promise<void> {
-
     for (const row of this.boardVerify) {
-
       for (const cell of row) {
+        const ball = this.allBalls.find(
+          x => x.id === cell.number
+        );
+        const success = cell.isDrawn;
+        cell.status = success
+          ? 'active-success'
+          : 'active-error';
+        if (ball) {
+          ball.status = success
+            ? 'active-success'
+            : 'active-error';
+        }
 
-        cell.status = cell.isDrawn
+        this.cdr.detectChanges();
+        await this.sleep(500);
+        cell.status = success
           ? 'success'
           : 'error';
 
-        await this.sleep(200);
-
+        if (ball) {
+          ball.status = success
+            ? 'success'
+            : 'error';
+        }
+        this.cdr.detectChanges();
       }
-
     }
-
   }
 
   sleep(ms: number): Promise<void> {
