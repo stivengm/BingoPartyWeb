ac7ad110357ec801fa11af518d7830b81c9ca5b6
Stiven Gomez <stivenalexander7u7@gmail.com>
2026-06-06 21:29:48 -0500

    FIX - Ajuste para que se pueda salir al winner el confeti completo

--- src/app/shared/winner/winner.ts
+++ src/app/shared/winner/winner.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfetiWinner } from '../confeti-winner/confeti-winner';
 
 @Component({
   selector: 'app-winner',
   imports: [
+    ConfetiWinner
   ],
   templateUrl: './winner.html',
   styleUrl: './winner.scss',
