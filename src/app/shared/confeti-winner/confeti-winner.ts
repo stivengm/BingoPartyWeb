26caf7364482b5ecff58956d030e95da51b054e7
Stiven Gomez <stivenalexander7u7@gmail.com>
2026-06-06 21:28:13 -0500

    ADD - Se añade componente de Confeti global.

---
+++ src/app/shared/confeti-winner/confeti-winner.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-confeti-winner',
+  imports: [],
+  templateUrl: './confeti-winner.html',
+  styleUrl: './confeti-winner.scss',
+})
+export class ConfetiWinner {
+  
+}
